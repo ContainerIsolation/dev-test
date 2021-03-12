71b0f865e7fbf11f6ab39c73766dbf78b221532a
Samuel Boczek <admin@samuelboczek.com>
2021-03-12 20:00:00 +0000

    Adds DataProviderInterface

---
+++ src/Model/DataProvider/DataProviderInterface.php
@@ -0,0 +1,15 @@
+<?php
+namespace Totallywicked\DevTest\DataProvider;
+
+/**
+ * A simple flexible interface that accepts data in array format and returns data in array format.
+ */
+interface DataProviderInterface
+{
+    /**
+     * Does some processing and returns data
+     * @param array $data
+     * @return array
+     */
+    function getData(array $data): array;
+}

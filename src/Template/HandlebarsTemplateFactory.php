fa267a822500f550e6e123458494b80914c46dc7
Samuel Boczek <admin@samuelboczek.com>
2021-03-12 20:00:00 +0000

    Adds HandlebarsTemplateFactory

---
+++ src/Template/HandlebarsTemplateFactory.php
@@ -0,0 +1,15 @@
+<?php
+namespace Totallywicked\DevTest\Template;
+
+use Totallywicked\DevTest\Factory\AbstractFactory;
+
+/**
+ * Factory for @see HandlebarsTemplate
+ */
+class HandlebarsTemplateFactory extends AbstractFactory
+{
+    /**
+     * @inheritDoc
+     */
+    protected $className = HandlebarsTemplate::class;
+}

346d38ebc0082d4337509b7ec95b7b7f62a94009
Samuel Boczek <admin@samuelboczek.com>
2021-03-12 20:00:00 +0000

    Tweaks to the model interface as per changes made

--- src/Model/ModelInterface.php
+++ src/Model/ModelInterface.php
@@ -5,7 +5,7 @@ namespace Totallywicked\DevTest\Model;
  * A very simple model interface that implements basic get data / set data methods.
  * Proper getters and setters are expected to be implemented inside the actual models.
  */
-interface ModelInterface extends \Serializable
+interface ModelInterface extends \Serializable, \ArrayAccess
 {
     /**
      * Return data from the set key or return the default value.
@@ -15,7 +15,7 @@ interface ModelInterface extends \Serializable
      * @param mixed $defaultValue
      * @return mixed
      */
-    function getData($key, $defaultValue = null);
+    function getData($key = null, $defaultValue = null);
 
     /**
      * Sets data on the given key.
@@ -26,5 +26,14 @@ interface ModelInterface extends \Serializable
      * @param mixed $value
      * @return self
      */
-    function setData($key, $value = null);
+    function setData($key = null, $value = null);
+
+    /**
+     * Calls unset on the given key.
+     * Calling without $key will unset all values.
+     * 
+     * @param string $key
+     * @return self
+     */
+    function unsetData($key = null);
 }

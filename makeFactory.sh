d800983ebf8d424f6f28f6f26ab3598a94e7f116
Samuel Boczek <admin@samuelboczek.com>
2021-03-12 20:00:00 +0000

    Improvements to the factory generator script

--- makeFactory.sh
+++ makeFactory.sh
@@ -11,6 +11,8 @@ fi
 
 CLASS_NAME="${FQN##*\\}"
 NAMESPACE="${FQN%\\*}"
+FILENAME="${FQN##Totallywicked\\DevTest\\}"
+FILENAME="src/${FILENAME//\\/\/}Factory.php"
 
 echo "\
 <?php
@@ -27,4 +29,4 @@ class ${CLASS_NAME}Factory extends AbstractFactory
      * @inheritDoc
      */
     protected \$className = $CLASS_NAME::class;
-}"
+}" > $FILENAME
